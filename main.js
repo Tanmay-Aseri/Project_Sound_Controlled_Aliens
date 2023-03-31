4ee7f9e1bd7a43cdb2e2b209f15969803186cb0c
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-03-31 13:42:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,5 +15,31 @@ function gotResults(error, results){
         console.log(error);
     } else {
       console.log(results);
+      random_number_r = Math.floor(Math.random() * 255) + 1;
+      random_number_g = Math.floor(Math.random() * 255) + 1;
+      random_number_b = Math.floor(Math.random() * 255) + 1;
+
+      document.getElementById("result_label").innerHTML = "I can hear "+ results[0].label;
+      document.getElementById("result_confidence").innerHTML =  "Accuracy - "+ (results[0].confidence*100).toFixed(2)+"%";
+      document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+
+      img = document.getElementById("Image");
+
+      if(results[0].label == "Barking") {
+        img.src = "dog.png";
+
+      } else if(results[0].label == "Mooing") {
+        img.src = 'cow.png';
+
+       }  else if(results[0].label == "Roar") {
+        img.src = 'lion.png';
+
+    }  else if(results[0].label == "Meowing") {
+        img.src = 'cat.png';
+
+       } else {
+        img.src = 'download.png';
+
     }
+}
 }
